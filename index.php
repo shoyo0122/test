ffc65724e0d9f6db382c4346eb38b5cc7b8a04d5
shoyo0122 <yoshizuka0122@gmail.com>
2021-04-25 14:21:18 +0900

    projectcalorie version1.0

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>カロリー計算</title>
+    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+    <header>
+        <h1>
+           <a href="/">カロリー計算</a>
+        </h1>
+        <!-- 
+        <nav class="pc-nav">
+           <ul>
+              <li><a href="login_form.php">ログイン</a></li>
+           </ul>
+        </nav>
+        -->
+     </header>
+     <div class="main-visual">
+     </div>
+</body>
+</html>
